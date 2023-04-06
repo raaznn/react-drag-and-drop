c1c3987ddf9003cee4a80d416365e5a619c8cd8f
raaznn <raaznn.sharma@gmail.com>
2023-04-06 09:37:01 +0545

    added deployment

--- src/hooks/useGrid.tsx
+++ src/hooks/useGrid.tsx
@@ -3,7 +3,7 @@ import { createContext, ReactNode, useContext, useState } from "react";
 const GridContext = createContext<any>(null);
 
 export const GridProvider = ({ children }: { children: ReactNode }) => {
-  const [numOfColumns, setNumOfColumns] = useState<number>(12);
+  const [numOfColumns, setNumOfColumns] = useState<number>(6);
   const [selectedGrid, setSelectedGrid] = useState<any>(null);
   const [gridList, setGridList] = useState<any[]>([]);
 
